abf0cb998e376cd973f7115c3d8faaac133ebf9f
Mayfly777w <2123344255@qq.com>
2023-06-18 16:27:05 +0800

    修改一些刚导入导致的bug

---
+++ Assets/Scripts/GameProcess/GameEntry.cs
@@ -0,0 +1,52 @@
+using cfg.MFramework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+/// <summary>
+/// Blog:AFoolZWT.github.io
+/// EMail:848832649@qq.com
+/// Time:2023/02/17
+/// Des£ºÓÎÏ·Èë¿Ú
+/// </summary>
+
+public class GameEntry : MonoBehaviour
+{
+    private bool Inited = false;
+
+    private void Start()
+    {
+        if (!Inited)
+        {
+            Init();
+        }
+    }
+
+    private void Update()
+    {
+        //test();
+    }
+
+    private void test()
+    {
+
+    }
+
+    private void Init()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        InitSingletons();
+
+        Debug.Log("GameEntry Init Completed");
+    }
+
+    private void InitSingletons()
+    {
+        ResMgr.Instance.Init();
+        CfgMgr.Instance.Init();
+        UIMgr.Instance.Init();
+        PoolMgr.Instance.Init();
+        TimerMgr.Instance.Init();
+        AudioMgr.Instance.Init();
+    }
+}
--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 一个基于Unity的游戏框架封装了一些开发过程中常用的方法
 
 ## 关于第三方包
-
 （1）使用了AddressableAsset包，它可以在unity内部直接通过包管理器下载
+关于它的使用我推荐这篇博客：https://blog.csdn.net/linxinfa/article/details/122390621
 
 （2）使用了Luban配置工具，它在Github上是开源的，以下是链接： https://github.com/focus-creative-games/luban
+关于它的使用我推荐这篇视频：https://www.bilibili.com/video/BV1xS4y1M7Aj
 
+## 
